63a1a346c43fa9b37beace49f9139e16db6de271
EvertonLDS815 <evertonlino1234@gmail.com>
2023-10-09 19:40:51 -0300

    my first commit

---
+++ src/pages/contato.tsx
@@ -0,0 +1,8 @@
+
+export default function Contato() {
+    return (
+        <>
+            <span>(87) 99157-6304</span>
+        </>
+    )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <>
+      <div>
+        <h1>Aprendendo Nextjs</h1>
+      </div>
+    </>
+  )
+}
