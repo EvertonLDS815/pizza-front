55e69a9a770d2b7bdbfdee7da44bae0225b12fee
EvertonLDS815 <evertonlino1234@gmail.com>
2023-10-10 12:38:19 -0300

    feat: Context api - created file AuthContext

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,28 @@ import Head from 'next/head';
 import Image from 'next/image'
 import styles from '../../styles/home.module.scss';
 import logoImg from '../../public/logo.svg';
+import {useContext, FormEvent} from 'react';
 
 import {Input} from '../components/ui/input';
 import { Button } from '@/components/ui/button';
 
+import {AuthContext} from '../context/authContext'
+
 import Link from 'next/link'
 
 export default function Home() {
+  const {signIn} = useContext(AuthContext);
+
+  async function handleLogin(event: FormEvent) {
+    event.preventDefault();
+
+    let data = {
+      email: 'evertonlino1234@gmail.com',
+      password: 'evertonqq123'
+    }
+
+    await signIn(data)
+  }
   return (
     <>
       <Head>
@@ -18,7 +33,7 @@ export default function Home() {
         <Image className={styles.logoImage} src={logoImg} alt="logo sujeito pizzaria" />
         
         <div className={styles.login}>
-          <form>
+          <form onSubmit={handleLogin}>
             <Input type="text" placeholder="Digite seu email" />
             <Input type="password" placeholder="Digite sua senha" />
             <Button type="submit" loading={false} >Acessar</Button>
