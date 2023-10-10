387aa65123f90dd41e32c86d3dc84947807abd54
EvertonLDS815 <evertonlino1234@gmail.com>
2023-10-10 08:48:27 -0300

    feat: add component button and its animated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import styles from '../../styles/home.module.scss';
 import logoImg from '../../public/logo.svg';
 
 import {Input} from '../components/ui/input';
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
@@ -19,6 +20,7 @@ export default function Home() {
         <form>
           <Input type="text" placeholder="Digite seu email" />
           <Input type="password" placeholder="Digite sua senha" />
+          <Button type="submit" loading={false} >Cadastrar</Button>
         </form>
       </div>
     </>
