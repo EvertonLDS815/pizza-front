6852cee1364c982d4fb0a34fb96ef07f2207d215
EvertonLDS815 <evertonlino1234@gmail.com>
2023-10-12 15:17:30 -0300

    feat: Remain login data

--- src/contexts/authContext.tsx
+++ src/contexts/authContext.tsx
@@ -1,4 +1,4 @@
-import {createContext, ReactNode, useState} from 'react';
+import {createContext, ReactNode, useState, useEffect} from 'react';
 import {api} from '../services/apiClient';
 
 import {destroyCookie, setCookie, parseCookies} from 'nookies';
@@ -45,6 +45,27 @@ export function signOut() {
 export function AuthProvider({children}: AuthProviderProps) {
     const [user, setUser] = useState<UserProps>()
     const isAuthenticated = !!user;
+
+    useEffect(() => {
+        // Tentar pegar algo na nookie
+        const {'@nextauth.token': token} = parseCookies()
+
+        if (token) {
+            api.get('/me').then(response => {
+                const {id, name, email} = response.data;
+
+                setUser({
+                    id,
+                    name,
+                    email
+                })
+            })
+            .catch(() => {
+                // Se deu erro deslogamos o user
+                signOut();
+            })
+        }
+    }, [])
 
     async function signIn({email, password}: SignInProps) {
         try {
