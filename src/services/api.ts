12ed9cb10b81584ac765d6fea80c72887c1cf4f6
EvertonLDS815 <evertonlino1234@gmail.com>
2023-11-07 19:25:18 -0300

    feat: add api link to deploy in the render

--- src/services/api.ts
+++ src/services/api.ts
@@ -7,7 +7,7 @@ export function setupAPIClient(ctx = undefined) {
   let cookies = parseCookies(ctx);
 
   const api = axios.create({
-    baseURL: 'http://localhost:3333',
+    baseURL: 'https://sujeito-pizzaria-el.onrender.com',
     headers: {
       Authorization: `Bearer ${cookies['@nextauth.token']}`,
     },
